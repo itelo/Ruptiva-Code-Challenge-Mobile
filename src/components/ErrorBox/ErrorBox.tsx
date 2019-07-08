9b59882a6ca1f2d4ea5013af429333b4a9abffe0
itelo filho <itelofilho@gmail.com>
2019-07-08 19:20:15 -0300

    add a component to help with error feedback

---
+++ src/components/ErrorBox/ErrorBox.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {View, Text} from "react-native"
+import { ThemeType } from "../../utils/styles/ThemeContext";
+import useStyles from "../../utils/styles/useStyles";
+
+
+
+type ErrorBoxProps = {
+  message: string
+}
+ 
+const ErrorBox = (props: ErrorBoxProps) => {
+  const styles = useStyles(classes);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        {props.message}
+      </Text>
+    </View>
+  );
+}
+
+const classes  = (theme: ThemeType) => ({
+  container:{
+    marginHorizontal: 48,
+    paddingVertical: 4,
+    paddingHorizontal: 4,
+    height: 48,
+    alignItems: "center" as "center",
+    justifyContent: "center" as "center",
+    flexDirection: "row" as "row",
+    backgroundColor: "#ffcdd2",
+    borderColor: "#b71c1c",
+    borderWidth: 2,
+    borderRadius: 4
+  },
+  text: {
+    ...theme.typography.body2,
+    color: "#b71c1c",
+  }
+});
+ 
+export default ErrorBox;
